b4605539e10296509b8fd71fd11d7bcf19e3d83a
Thushara051989 <102418353+Thushara051989@users.noreply.github.com>
2022-07-07 22:39:26 +0530

    changes

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,20 @@
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-about',
+//   templateUrl: './about.component.html',
+//   styleUrls: ['./about.component.css']
+// })
+// export class AboutComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit(): void {
+//   }
+
+// }
 import { Component, OnInit } from '@angular/core';
+import * as awesom from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-about',
@@ -6,10 +22,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  mapMarker: any;
+  building: any;
+  mobile: any;
+  email: any;
+  birthday: any;
+  badge: any;
+  home: any;
+  
+  constructor() {
+    
+  }
 
   ngOnInit(): void {
+    this.mapMarker = awesom.faMapMarker;
+    this.building = awesom.faCity;
+    this.email = awesom.faMailBulk;
+    this.mobile = awesom.faMobile;
+    this.birthday = awesom.faBirthdayCake;
+    this.badge = awesom.faGraduationCap;
+    this.home = awesom.faLaptopHouse;
   }
 
 }
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,20 @@
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-contact',
+//   templateUrl: './contact.component.html',
+//   styleUrls: ['./contact.component.css']
+// })
+// export class ContactComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit(): void {
+//   }
+
+// }
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +23,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+   
+  
   }
 
+  ngOnInit(): void {
+   
+  }
 }
