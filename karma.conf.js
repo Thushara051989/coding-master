3620c34ff3f84aafb508748b3f1d536fd3f0668e
Thushara051989 <102418353+Thushara051989@users.noreply.github.com>
2022-07-10 00:17:50 +0530

    cha

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/coding'),
+      dir: require('path').join(__dirname, './coverage/coding-master'),
       subdir: '.',
       reporters: [
         { type: 'html' },
